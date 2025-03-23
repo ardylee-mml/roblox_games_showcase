94ef10f0a1d5a207ed5594af163969ee5a5c1b96
Ardy Lee <ardylee.mml@gmail.com>
2025-03-23 17:19:32 +0800

    fix: update theme provider types to match next-themes requirements

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,16 @@
 'use client'
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ReactNode } from 'react'
+
+type Attribute = 'class' | 'data-theme'
+
+interface ThemeProviderProps {
+  children: ReactNode;
+  attribute?: Attribute | Attribute[];
+  defaultTheme?: string;
+  enableSystem?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
